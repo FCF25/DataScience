6bd62b009eb6a7adcc3463c8b8619528d0bec451
Fabio Freitas <fabio_contuti@hotmail.com>
2022-05-13 16:08:17 -0300

    Analise dos dados do TMDB - Grafico com todas as linguas sem contar a lingua inglesa.

---
+++ analise_tmdb.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+tmdb = pd.read_csv('dados/tmdb_5000_movies.csv')
+# tmdb.columns = ['orcamento', 'genero', 'homepage', 'id', 'keywords', 'lingua_original','titulo_original', 'overview', 'popularidade', 'production_companies','production_countries', 'release_date', 'revenue', 'runtime','spoken_languages', 'status', 'tagline', 'title', 'vote_average', 'vote_count']
+# print(tmdb.columns)
+# print(tmdb.original_language.unique()) # retorna os valores unicos das linguas dos filmes sem repetir.
+# print(tmdb.original_language.value_counts().to_frame()) # To_frame() = Vamos ter o indice e uma colona
+# print(tmdb.original_language.value_counts().reset_index())# reset_index() = Desta maneira vamos ter duas colunas pois tiramos o index. 1 lingua e 2 quantidade de vezes que foi escrita.
+# contagem_de_lingua = tmdb.original_language.value_counts().reset_index()  # colocamos dentro de uma variável o nosso resultado das linguas e suas contagens.
+# contagem_de_lingua.columns = ["original_language","total"]  # colocamos um nome em cada celula, no caso colocamos um titulo em cada uma.
+# # print(contagem_de_lingua.head())
+#
+# #Criando um grafico com as informações:
+# sns.set()
+# #BARPLOT é um grafico de baixo nivel, onde que com ele conseguimos ter mais controle do que queremos fazer
+# sns.barplot(x="original_language", y="total", data=contagem_de_lingua) # Criamos um grafico com as linguas na horizontal e na vertical colocamos as quantidades que foram colocadas nos dados.
+#
+# #CATPLOT é um grafico de alto nivel, onde ele tem maneiras automaticas e configuraveis de carregar o grafico.
+# sns.catplot(x="original_language", kind = "count" , data=tmdb) # ele gera o grafico, mas não é de forma ordenada conforme o outro grafico, pois no barplot nós fizemos o separamento de tudo e no catplot nós pegamos os dados crus.
+#
+# #GRAFICO DE PIZZA
+# plt.pie(contagem_de_lingua["total"], labels= contagem_de_lingua["original_language"]) # O grafico é muito ruim para se trabalhar com bastante dados. Raramente usaremos.
+# plt.show()
+
+# isolando os dados.
+total_por_lingua = tmdb["original_language"].value_counts() # irá nos retornar a lingua e sua contagem
+total_geral = total_por_lingua.sum()#SUM vai somar todos os valores e nos dar um total.
+total_de_ingles = total_por_lingua.loc["en"]#LOC["EN"] vai localizar em nossa lista apenas a lingua INGLES e sua quantidade.
+total_do_resto = total_geral - total_de_ingles# Vai pegar o total de linguas menos a lingua inglesa.
+# print(total_do_resto)
+
+#Criamos um dicionario com os dados para que fiquem separados e assim consigamos colocar em um grafico a diferença entre o ingles e todos os outros.
+dados = {
+    'lingua': ['ingles','outros'],
+    'total':[total_de_ingles,total_do_resto]
+}
+dados = pd.DataFrame(dados)
+# print(dados)
+# sns.barplot(x="lingua", y="total", data= dados) # Grafico de barra com os dois valores
+# plt.pie(dados["total"], labels= dados["lingua"])# Grafico de pizza/torta com os dois valores
+plt.show()
+
+#Buscar coisas dentro do dataframe = Query ()
+total_de_lingua_de_outros_filmes = tmdb.query("original_language != 'en'").original_language.value_counts() # irá nos mostrar a contagem de todas as linguas menos o ingles.
+# print(total_de_lingua_de_outros_filmes)
+
+
+filmes_sem_lingua_original_em_ingles = tmdb.query("original_language != 'en'")
+sns.catplot(x = "original_language", kind="count", data= filmes_sem_lingua_original_em_ingles)
+plt.title("Linguas Originail de filmes")
+plt.show()
+
